be6701f1a83ea2f17a69adfce4606c0d2fc417a3
kxngpudz <71802085+kxngpudz@users.noreply.github.com>
2023-07-29 16:04:24 -0400

    Update buy.tsx

--- pages/buy.tsx
+++ pages/buy.tsx
@@ -11,13 +11,13 @@ export default function Buy() {
 
   return (
     <Container maxWidth="lg">
-      <h1>Buy NFTs</h1>
-      <p>Browse which NFTs are available from the collection.</p>
+      <h1>IP showcase</h1>
+      <p>Browse the different intellectual property create with our platform</p>
       <NFTGrid
         data={data}
         isLoading={isLoading}
         emptyText={
-          "Looks like there are no NFTs in this collection. Did you import your contract on the thirdweb dashboard? https://thirdweb.com/dashboard"
+          "Looks like there are no NFTs in this collection."
         }
       />
     </Container>
